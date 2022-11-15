cf4b5fd41a04d05f01278dccbdc1eb27789ca170
Dylan Hamersztein <dylanhamersztein@gmail.com>
2022-11-15 23:08:54 +0000

    Load flags asynchronously.

--- src/features/flag/Flag.tsx
+++ src/features/flag/Flag.tsx
@@ -1,17 +1,33 @@
 import "./Flag.scss";
 import { useAppSelector } from "../../app/hooks";
+import { useEffect, useState } from "react";
 
 type Props = {};
 export const Flag = (props: Props) => {
   const countryCode = useAppSelector(
     (state) => state.game.currentRound.answer?.code
   );
+
+  const [flagObjectUrl, setFlagObjectUrl] = useState<any>(undefined);
+
+  useEffect(() => {
+    fetch("https://countryflagsapi.com/png/" + countryCode)
+      .then((res) => res.blob())
+      .then(URL.createObjectURL)
+      .then(setFlagObjectUrl);
+  }, [countryCode, setFlagObjectUrl]);
+
   return (
-    <img
-      style={{ marginTop: "5vh" }}
-      className="question-flag"
-      src={"https://countryflagsapi.com/png/" + countryCode}
-      alt="Which flag is this?"
-    />
+    <>
+      {flagObjectUrl ? (
+        <img
+          alt="Which country's flag is this?"
+          className="question-flag"
+          src={flagObjectUrl}
+        />
+      ) : (
+        <div className="loading-icon"></div>
+      )}
+    </>
   );
 };
